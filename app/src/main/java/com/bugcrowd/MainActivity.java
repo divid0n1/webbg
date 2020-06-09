6d54d3f11bf5ae40037da4ed6b58cb7c90f2bcaf
devid0n1@outlook.com <kjS7YVsU42XpRvE>
2020-06-09 14:11:54 +0500

    First Commit

---
+++ app/src/main/java/com/bugcrowd/MainActivity.java
@@ -0,0 +1,28 @@
+package com.bugcrowd;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    private String webUrl = "https://www.bugcrowd.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.myWebView);
+        webView.loadUrl(webUrl);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+}
