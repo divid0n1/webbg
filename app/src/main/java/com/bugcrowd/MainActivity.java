24d3691874d65f06397be3fafea180de80f36762
devid0n1@outlook.com <kjS7YVsU42XpRvE>
2020-06-10 16:36:13 +0500

    Including Check Connectivity & ProgressBar

--- app/src/main/java/com/bugcrowd/MainActivity.java
+++ app/src/main/java/com/bugcrowd/MainActivity.java
@@ -1,17 +1,35 @@
 package com.bugcrowd;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
+
+
     private String webUrl = "https://www.bugcrowd.com";
+    ProgressBar progressBarWeb;
+   // ProgressDialog progressDialog;
+    RelativeLayout relativeLayout;
+    Button btnNoInternetConnection;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +37,14 @@ public class MainActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
         webView = (WebView) findViewById(R.id.myWebView);
+        progressBarWeb = (ProgressBar) findViewById(R.id.progressBar) ;
+        //progressDialog = new ProgressDialog(context: this);
+       // progressDialog.setMessage("Loading Please Wait");
+        btnNoInternetConnection = (Button) findViewById(R.id.btnNoConnection);
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        checkConnection();
         webView.loadUrl(webUrl);
         webView.setWebViewClient(new WebViewClient(){
             @Override
@@ -28,6 +53,31 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                progressBarWeb.setVisibility(View.VISIBLE);
+                progressBarWeb.setProgress(newProgress);
+                setTitle("Loading...");
+                if(newProgress ==100){
+                    progressBarWeb.setVisibility(View.GONE);
+                    setTitle(view.getTitle());
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+        btnNoInternetConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkConnection();
+            }
+        });
+
+
+
     }
 
     @Override
@@ -38,4 +88,26 @@ public class MainActivity extends AppCompatActivity {
         else
         super.onBackPressed();
     }
+    public void checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+        if (wifi.isConnected()) {
+            webView.loadUrl(webUrl);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+
+        } else if (mobileNetwork.isConnected()) {
+            webView.loadUrl(webUrl);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+
+        } else {
+            webView.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
